5a135b7a26b9db777adbdb09a96fd426be9825ff
Andrej Antas <andrej@antas.cz>
2021-03-07 09:03:47 +0100

    Tyring to debug something in app.... sry

--- lib/gp_webpay.rb
+++ lib/gp_webpay.rb
@@ -6,4 +6,13 @@ require "gp_webpay/verification"
 require "gp_webpay/web_services"
 
 module GpWebpay
+  class << self
+    attr_writer :logger
+
+    def logger
+      @logger ||= Logger.new($stdout).tap do |log|
+        log.progname = self.name
+      end
+    end
+  end
 end
